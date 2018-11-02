e399a66c8d23dd57686990c9987797275709c570
polinca <polinca@bk.ru>
2018-11-02 20:16:41 +0500

    first

---
+++ b/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  b
+//
+//  Created by Litvinova Polina on 02/11/2018.
+//  Copyright © 2018 Litvinova Polina. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ b/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  b
+//
+//  Created by Litvinova Polina on 02/11/2018.
+//  Copyright © 2018 Litvinova Polina. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
